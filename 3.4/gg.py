bf58e4f3c67dbf6e2765a8525621d1d3b95b6c7b
porkblade <maxlukonin@gmail.com>
2026-02-02 16:20:12 +0300

    feature: делал задачу E

--- 3.4/gg.py
+++ 3.4/gg.py
@@ -1,1 +1,7 @@
-print("heloo")
+full_list = []
+for _ in range(3):
+    list_part = input().split(', ')
+    for item in list_part:
+        full_list.append(item)
+for index, value in enumerate(sorted(full_list), 1):
+    print(f'{index}. {value} ')
